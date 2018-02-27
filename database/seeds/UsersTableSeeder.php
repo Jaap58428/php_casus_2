36f96ea1037e7f36f2d5ae3fa72f898daa42c43a
Jaap58428 <jaapkanbier@hotmail.com>
2018-02-27 13:02:52 +0100

    custom css

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
    public function run()
    {
      $user_list = [
-       ['name' => 'Jaap Kanbier', 'student_nummer' => 's1100592', 'email' => 'jaapkanbier@hotmail.com', 'password' => bcrypt('qwerty123')],
+       ['name' => 'Jaap Kanbier', 'student_nummer' => 's1100592', 'email' => 'jaap@gmail.com', 'password' => bcrypt('qwerty123')],
        ['name' => 'John Doe', 'student_nummer' => 's1100123', 'email' => 'somebody@hotmail.com', 'password' => bcrypt('secret123')],
        ['name' => 'D. Ocent', 'email' => 'belangrijk@hotmail.com', 'password' => bcrypt('wachtwoord123'), 'is_admin' => true],
      ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Studie Dashboard</title>
+        <title>{{ config('app.name', 'Studie Dashboard') }}</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="welcome-body">
         <div class="welcome-left">
           <h1>Studie Dashboard!</h1>
           <h2>Het middelpunt van jouw voortgang</h2>
         </div>
         <div class="welcome-right">
           @if (Route::has('login'))
-              <div>
+              <div class="welcome-login">
                   @auth
                       <a href="{{ url('/home') }}">Home</a>
                   @else
