1d4d726550c2208384b2d65c828a2adbb5fbdc95
Jaap58428 <jaapkanbier@hotmail.com>
2018-02-28 15:02:43 +0100

    added custom font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> 
 </head>
 <body>
     <div id="app" class="wrapper">
