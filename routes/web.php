86fd3a5f7378bca4c1b7871405e22e977c5a8eb4
Jaap58428 <jaapkanbier@hotmail.com>
2018-03-01 16:30:39 +0100

    laravel collective installed

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
